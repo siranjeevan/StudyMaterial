1befd6d655a695281b648cda73cd514e69738b0d
siranjeevan <smartjeevith2k@gamil.com>
2025-01-26 22:01:15 +0530

    Initial Commit

---
+++ StudyMaterial/StudyMaterialApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyMaterialApp.swift
+//  StudyMaterial
+//
+//  Created by Jeevith  on 26/01/25.
+//
+
+import SwiftUI
+
+@main
+struct StudyMaterialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
