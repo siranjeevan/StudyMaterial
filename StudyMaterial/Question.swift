b324fc9583afc7e4f81fceb4dd55d364a5f42ace
siranjeevan <smartjeevith2k@gamil.com>
2025-02-01 12:44:46 +0530

    question game

--- StudyMaterial/Question.swift
+++ StudyMaterial/Question.swift
@@ -1,24 +1,57 @@
 import SwiftUI
 
 struct Question: View {
-    let Question : [String] = ["What does CPU stand for?"]
-    let options = [
-        "A) Central Processing Unit",
-        "B) Computer Processing Unit",
-        "C) Central Program Unit",
-        "D) Central Power Unit"
+    let Question : [String] = [
+        "What does CPU stand for?",
+        "Which of the following is a programming language?",
+        "What is the main function of an operating system?",
+        "What does HTTP stand for?",
+        "Which data structure uses LIFO (Last In, First Out) principle?"
     ]
-    func style() -> some View {
+    let options :[[String]] = [["A) Central Processing Unit", "B) Computer Processing Unit", "C) Central Program Unit", "D) Central Power Unit"],
+    ["A) HTML", "B) Python", "C) HTTP", "D) CSS"],
+    ["A) To manage hardware and software resources", "B) To create websites", "C) To perform arithmetic calculations", "D) To design graphics"],
+    ["A) Hyper Text Transfer Protocol", "B) High Technology Transmission Protocol", "C) Hyperlink Transfer Process", "D) Hyper Transfer Text Program"],
+    ["A) Queue", "B) Stack", "C) Linked List", "D) Array"]
+]
+    let correctAnswers = [
+        "A) Central Processing Unit",
+        "B) Python",
+        "A) To manage hardware and software resources",
+        "A) Hyper Text Transfer Protocol",
+        "B) Stack"
+    ]
+    @State private var isAnimating = false
+    @State var questionNumber = 0
+    @State var optionIndex = 0
+    @State var score = 0
+    @State var varifyAnswer = ""
+    func style(i : Int ) -> some View {
         Button {
-            
+            varifyAnswer = options[optionIndex][i]
+            isAnimating.toggle()
+            answerVerify()
         } label: {
-            Text("Text")
+            Text("\(options[optionIndex][i])")
+                .foregroundStyle(varifyAnswer == correctAnswers[questionNumber] ? .green : .red)
+                .animation(.easeInOut(duration: 1), value: isAnimating)
                 .frame(width: 380, height: 70)
                 .background(Color.white)
                 .cornerRadius(20)
             .shadow(radius: 10)
             .padding(.top , 5)
         }
+    }
+    func answerVerify() {
+        if varifyAnswer == correctAnswers[questionNumber] {
+            score += 1
+        }
+    }
+    
+    func result() -> some View {
+        Text("Score: \(score)/\(Question.count)")
+            .font(.title)
+            .foregroundStyle(.blue)
     }
     var body: some View {
         Text("Question")
@@ -35,7 +68,7 @@ struct Question: View {
             .offset(x: 0, y: -250)
         )
         .overlay(
-            Text("\(Question[0])")
+            Text("\(Question[questionNumber])")
                 .font(.headline)
                 .frame(width: 380, height: 150)
                 .foregroundColor(.black)
@@ -43,18 +76,12 @@ struct Question: View {
                 .offset(x: 0, y: -250)
             )
         .overlay(
-            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 0), count: 1), spacing: 10)
-            {
-                ForEach(0..<4) { _ in
-                    style()
-                }
-            }
-        )
-        .overlay(
             Button(action: {
+                    questionNumber += 1
+                    optionIndex += 1
                 
             }, label: {
-                Text("Check")
+                Text("Next")
                     .frame(width: 100, height: 40)
                     .background(Color.white)
                     .cornerRadius(20)
@@ -63,6 +90,20 @@ struct Question: View {
         )
             .offset(x: 0, y: 350)
             )
+        .overlay(
+            VStack
+            {
+                if questionNumber < Question.count - 1  {
+                    ForEach(0..<4) { i in
+                            style(i : i)
+                    }
+                }
+                else
+                {
+                    result()
+                }
+            }
+        )
     }
 }
 
